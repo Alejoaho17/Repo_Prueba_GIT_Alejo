c9982b9487168f1b84005217a2287e6644451c48
AlejoSHernandez <alejo.hernandez@correo.unimet.edu.ve>
2024-10-23 17:25:10 -0400

    Agregamos clase de prueba

---
+++ Prueba_GIT_Alejo_(2)/src/prueba_git_alejo_/pkg2/Clase_Alejo_Ralejo_hernandez.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba_git_alejo_.pkg2;
+
+/**
+ *
+ * @author Alejo_Samuel
+ */
+public class Clase_Alejo_Ralejo_hernandez {
+    
+}
