c60a8b4a4251d28200830f5ba697325d9b76753b
AlejoSHernandez <alejo.hernandez@correo.unimet.edu.ve>
2024-10-23 17:15:37 -0400

    Carpeta inicio

---
+++ Prueba_GIT_Alejo_(2)/src/prueba_git_alejo_/pkg2/Prueba_GIT_Alejo_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba_git_alejo_.pkg2;
+
+/**
+ *
+ * @author Alejo_Samuel
+ */
+public class Prueba_GIT_Alejo_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
